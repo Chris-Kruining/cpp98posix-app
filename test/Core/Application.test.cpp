79d3769c162671f96a3e0921e3ad495f97f34bf2
Chris P Bacon <chris@kruining.eu>
2017-02-13 22:16:31 +0100

    removed test files from the framework

--- test/Core/Application.test.cpp
+++
@@ -1,8 +0,0 @@
-#define BOOST_TEST_MODULE test_Core_Application
-#include <boost/test/unit_test.hpp>
-#include "../../src/Core/Application.h"
-
-void test_Core_Application()
-{
-    BOOST_CHECK(1 == 1);
-}
